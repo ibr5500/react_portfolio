2c3279b56d061ee5abc823af549ae0c5d2b0b2cc
IBRAHIM AHMAT <ibr.s.ahmat@gmail.com>
2022-10-27 13:38:53 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import About from './components/About'; // eslint-disable-line
-import Contact from './components/Contact'; // eslint-disable-line
-import Home from './components/Home'; // eslint-disable-line
-import Navbar from './components/Navbar'; // eslint-disable-line
-import Works from './components/Works'; // eslint-disable-line
+import About from './Components/About'; // eslint-disable-line
+import Contact from './Components/Contact'; // eslint-disable-line
+import Home from './Components/Home'; // eslint-disable-line
+import Navbar from './Components/Navbar'; // eslint-disable-line
+import Works from './Components/Works'; // eslint-disable-line
 
 function App() {
   return (
