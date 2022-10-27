f8aa0dba26d23b677d43e91dad4e275e01653d2b
unknown <ibr.s.ahmat@gmail.com>
2022-10-27 15:04:45 +0300

    Fix an onreload erorr

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
     <BrowserRouter>
       <Navbar />
       <Routes>
+        <Route path="/reload" component={null} key="reload" />
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
