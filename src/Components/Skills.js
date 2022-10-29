aef32e2bd91c5bca3771ea05cc30eb46ea795fd6
unknown <ibr.s.ahmat@gmail.com>
2022-10-29 23:45:39 +0300

    Pulishing the about page

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -10,28 +10,31 @@ import { BsFillBootstrapFill } from 'react-icons/bs';
 
 const Skills = () => {
   const langs = [
-    { id: 1, name: 'git', icon: <FaGitAlt /> },
-    { id: 2, name: 'html', icon: <FaHtml5 /> },
-    { id: 3, name: 'css', icon: <FaCss3Alt /> },
-    { id: 4, name: 'javaScript', icon: <FaJsSquare /> },
-    { id: 5, name: 'webpack', icon: <SiWebpack /> },
-    { id: 6, name: 'react', icon: <FaReact /> },
-    { id: 7, name: 'redux', icon: <SiRedux /> },
-    { id: 8, name: 'bootStrap', icon: <BsFillBootstrapFill /> },
-    { id: 9, name: 'jest', icon: <SiJest /> },
-    { id: 10, name: 'ruby', icon: <DiRuby /> },
-    { id: 11, name: 'rails', icon: <SiRubyonrails /> },
-    { id: 12, name: 'postgresql', icon: <SiPostgresql /> },
-    { id: 13, name: 'mysql', icon: <SiMysql /> },
+    { id: 1, name: 'html', icon: <FaHtml5 /> },
+    { id: 2, name: 'css', icon: <FaCss3Alt /> },
+    { id: 3, name: 'javaScript', icon: <FaJsSquare /> },
+    { id: 4, name: 'ruby', icon: <DiRuby /> },
   ];
+
+  const frams = [
+    { id: 1, name: 'git', icon: <FaGitAlt /> },
+    { id: 2, name: 'webpack', icon: <SiWebpack /> },
+    { id: 3, name: 'react', icon: <FaReact /> },
+    { id: 4, name: 'redux', icon: <SiRedux /> },
+    { id: 5, name: 'bootStrap', icon: <BsFillBootstrapFill /> },
+    { id: 6, name: 'jest', icon: <SiJest /> },
+    { id: 7, name: 'rails', icon: <SiRubyonrails /> },
+    { id: 8, name: 'postgresql', icon: <SiPostgresql /> },
+    { id: 9, name: 'mysql', icon: <SiMysql /> },
+  ];
+
+  // const profs = [];
 
   return (
     <section className="file-marker">
       <div>
         <div className="box-title">
-          <p>
-            skills
-          </p>
+          skills
         </div>
         <div className="skills">
           <div className="skill-header d-flex flex-column">
@@ -42,7 +45,7 @@ const Skills = () => {
               </a>
               <div className="">
                 <div className="collapse multi-collapse" id="collapseExample">
-                  <div className="skills-list gap-2 pt-2 text-left">
+                  <div className="skills-list gap-2 p-2 text-left border-b">
                     {langs.map((skill) => (
                       <div
                         key={skill.id}
@@ -56,15 +59,21 @@ const Skills = () => {
                 </div>
               </div>
               <a className="d-flex drop-btn p-2 justify-content-between" data-bs-toggle="collapse" href="#collapseExample2" role="button" aria-expanded="false" aria-controls="collapseExample2">
-                Frameworks
+                Frameworks & tools
                 <FaAngleDown />
               </a>
               <div className="">
                 <div className="collapse multi-collapse" id="collapseExample2">
-                  <div className="skills-list gap-3 text-left">
-                    <div>
-                      React
-                    </div>
+                  <div className="skills-list gap-2 p-2 text-left border-b">
+                    {frams.map((skill) => (
+                      <div
+                        key={skill.id}
+                        className={skill.name}
+                      >
+                        {skill.icon}
+                        <span className="px-2">{skill.name}</span>
+                      </div>
+                    ))}
                   </div>
                 </div>
               </div>
@@ -73,7 +82,7 @@ const Skills = () => {
                 <FaAngleDown />
               </a>
               <div className="">
-                <div className="collapse multi-collapse" id="collapseExample3">
+                <div className="border-t collapse multi-collapse" id="collapseExample3">
                   <div>
                     skills
                   </div>
