e9cbc53359912c2ed42cb10c9db15d02bdd43dcf
unknown <ibr.s.ahmat@gmail.com>
2022-10-31 01:40:50 +0300

    Add google analytics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ReactGA from 'react-ga';
 import './App.css';
 import About from './Components/About'; // eslint-disable-line
 import Contact from './Components/Contact'; // eslint-disable-line
 import Home from './Components/Home'; // eslint-disable-line
 import Navbar from './Components/Navbar'; // eslint-disable-line
 import Works from './Components/Works'; // eslint-disable-line
+
+const trackID = 'UA-247694122-1'; // my_track_ID
+ReactGA.initialize(trackID);
+ReactGA.pageview(window.location.pathname);
 
 function App() {
   return (
