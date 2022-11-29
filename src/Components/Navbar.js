f2450a4560b42074e9bccc83bf123d5b0d0669ee
unknown <ibr.s.ahmat@gmail.com>
2022-11-29 14:38:56 +0300

    Fix linters error

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,23 +1,48 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+// import { FaBars } from 'react-icons/fa';
 
-const Navbar = () => (
-  <nav className="nav">
-    <NavLink to="/">
-      <img className="rounded-circle recomend-img logo-img" src="./assets/logo.png" alt="Ibrahim Ahmat" />
-    </NavLink>
-    <ul className="nav-list">
-      <li className="nav-item">
-        <NavLink className="navbar-link" to="/about">About</NavLink>
-      </li>
-      <li className="nav-item">
-        <NavLink className="navbar-link" to="/projects">Projects</NavLink>
-      </li>
-      <li className="nav-item">
-        <NavLink className="navbar-link" to="/contact">Contact</NavLink>
-      </li>
-    </ul>
-  </nav>
-);
+function Navbar() {
+  return (
+    <nav className="nav">
+      <NavLink to="/">
+        <img
+          className="rounded-circle recomend-img logo-img"
+          src="./assets/logo.png"
+          alt="Ibrahim Ahmat"
+        />
+      </NavLink>
+      <ul className="nav-list">
+        <li className="nav-item">
+          <NavLink
+            className="navbar-link"
+            to="/about"
+          >
+            About
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink
+            className="navbar-link"
+            to="/projects"
+          >
+            Projects
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink
+            className="navbar-link"
+            to="/contact"
+          >
+            Contact
+          </NavLink>
+        </li>
+        {/* <li>
+          <FaBars className="menubar" />
+        </li> */}
+      </ul>
+    </nav>
+  );
+}
 
 export default Navbar;
